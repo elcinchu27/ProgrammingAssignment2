923dfc20374c70741a7ed6ee785cc755689c0844
elcinchu27 <elcinchu27@gmail.com>
2018-08-14 19:22:48 +0200

    edit cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+#R script that is able to cache potentially time-consuming computations
 
-## Write a short comment describing this function
+#makeCacheMatrix builds a set of functions and returns the functions within a list to the parent environment
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y         
+                m <<- NULL      
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve  
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+#cacheSolve is required to populate and/or retrieve the mean from an object
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
